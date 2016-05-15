652d20e40f42a6e7dd3a299670cf1e986c0e81b7
Graeme Mathieson <mathie@woss.name>
2016-05-15 17:39:54 +0100

    Get the ISO8601 date right...

--- config.rb
+++ config.rb
@@ -77,6 +77,11 @@ helpers do
       link_to title, url, options
     end
   end
+
+  def xml_timestamp(dateish = Time.now)
+    timestamp = dateish.respond_to?(:strftime) ? dateish : Date.parse(dateish)
+    timestamp.strftime('%FT%H:%M:%S%:z')
+  end
 end
 
 # Build-specific configuration
